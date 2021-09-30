2554164eef4303d828fdebea8dd2dcef06ea7ea3
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-09-30 14:31:03 -0400

    offline functionality

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -2,7 +2,7 @@ let db;
 
 const request = indexedDB.open("budget_tracker", 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+// this event will emit if the database version changes (nonexistent to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
     // save a reference to the database
     const db = event.target.result;
@@ -16,7 +16,7 @@ request.onsuccess = function (event) {
     db = event.target.result;
 
     if (navigator.onLine) {
-        // uploadBudget(); 
+        uploadBudget(); 
     }
 };
 
@@ -31,4 +31,40 @@ function saveRecord(record) {
     const budgetObjectStore = transaction.objectStore("update_budget");
 
     budgetObjectStore.add(record);
+};
+
+function uploadBudget() {
+    const transaction = db.transaction(["update_budget"], "readwrite");
+
+    const budgetObjectStore = transaction.objectStore("update_budget");
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                },
+            })
+            .then(response => response.json())
+            .then((serverResponse) => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction([update_budget], 'readwrite');
+                const budgetObjectStore = transaction.objectStore('update_budget');
+
+                alert('Budget has been updated!')
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        }
+    };
 }
+
+window.addEventListener('online', uploadBudget);
